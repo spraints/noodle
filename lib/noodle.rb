0b8635bdee4ef832b680db6ca2d8b2db4a5eedef
Matt Burke <spraints@gmail.com>
2010-08-27 00:33:25 -0400

    Added a command line interface outside of rake.

--- lib/noodle.rb
+++ lib/noodle.rb
@@ -1,29 +1,9 @@
 require 'rake/tasklib'
 require 'bundler'
+require 'forwardable'
+
 module Noodle
-  module Rake
-
-  # Rake tasks for setting up .NET dependencies.
-  #
-  # Four tasks are defined:
-  # * noodle:copy - copy the lib directories from the gems to the local project.
-  # * noodle:clean - remove the local lib directory.
-  # * noodle:update - clean, then copy.
-  # * noodle - alias of noodle:copy.
-  #
-  # @example
-  #   Noodle::Rake::NoodleTask.new :customname do |n|
-  #     n.groups << :dotnet # only use the bundler :dotnet group
-  #     n.outdir = 'someplace-other-than-lib'
-  #     n.merge! # merge all the libs together
-  #   end
-  class NoodleTask < ::Rake::TaskLib
-    # The name of the rake task to generate.
-    #
-    # Defaults to :noodle.
-    # @return [String]
-    attr_accessor :name
-
+  class Noodler
     # An array of bundler groups that reference the required dlls.
     # Use an empty array for all groups.
     #
@@ -52,14 +32,73 @@ module Noodle
       @merge = true
     end
 
+    def initialize
+      @groups = []
+      @outdir = 'lib'
+      @merge  = false
+    end
+
+    def install
+      specs = groups.any? ? Bundler.definition.specs_for(groups) : Bundler.environment.requested_specs
+      specs.each do |spec|
+        next if spec.name == 'bundler'
+        spec.load_paths.each do |path|
+          dest_path = dest_for(spec)
+          FileUtils.mkdir_p dest_path
+          FileUtils.cp_r "#{path}/.", dest_path, :verbose => true
+        end
+      end
+    end
+
+    private
+    def dest_for spec
+      dest = outdir.dup
+      dest << "/#{spec.name}-#{spec.version}" unless merge
+      File.expand_path(dest)
+    end
+  end
+
+  module Rake
+
+  # Rake tasks for setting up .NET dependencies.
+  #
+  # Four tasks are defined:
+  # * noodle:copy - copy the lib directories from the gems to the local project.
+  # * noodle:clean - remove the local lib directory.
+  # * noodle:update - clean, then copy.
+  # * noodle - alias of noodle:copy.
+  #
+  # @example Gemfile
+  #   source :rubygems
+  #   group :dotnet do
+  #     gem 'structuremap'
+  #   end
+  #
+  # @example Rakefile
+  #   Noodle::Rake::NoodleTask.new :customname do |n|
+  #     n.groups << :dotnet # only use the bundler :dotnet group
+  #     n.outdir = 'someplace-other-than-lib'
+  #     n.merge! # merge all the libs together
+  #   end
+  class NoodleTask < ::Rake::TaskLib
+    # The name of the rake task to generate.
+    #
+    # Defaults to :noodle.
+    # @return [String]
+    attr_accessor :name
+
+    extend Forwardable
+    def_delegators :@noodler,
+      :groups=, :groups,
+      :outdir=, :outdir,
+      :merge=,  :merge,  :merge!
+
     # Define noodle's rake tasks.
     # @yield [noodle]
     # @yieldparam [NoodleTask] noodle the noodle task generator.
     def initialize name = :noodle
-      @name   = name
-      @groups = []
-      @outdir = 'lib'
-      @merge  = false
+      @noodler = Noodler.new
+      @name    = name
       yield self if block_given?
       define
     end
@@ -73,15 +112,7 @@ module Noodle
 
       desc "Copies .NET dependencies to '#{outdir}'"
       task "#{name}:copy" do
-        specs = groups.any? ? Bundler.definition.specs_for(groups) : Bundler.environment.requested_specs
-        specs.each do |spec|
-          next if spec.name == 'bundler'
-          spec.load_paths.each do |path|
-            dest_path = dest_for(spec)
-            FileUtils.mkdir_p dest_path
-            FileUtils.cp_r "#{path}/.", dest_path, :verbose => true
-          end
-        end
+        @noodler.install
       end
 
       desc "Cleans '#{outdir}'"
@@ -91,12 +122,6 @@ module Noodle
 
       desc "Updates .NET dependencies in '#{outdir}'"
       task "#{name}:update" => ["#{name}:clean", "#{name}:copy"]
-    end
-
-    def dest_for spec
-      dest = outdir.dup
-      dest << "/#{spec.name}-#{spec.version}" unless merge
-      File.expand_path(dest)
     end
   end
 
